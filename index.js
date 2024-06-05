f9f81512daff0f50812b60313f3e1d769cc60546
Sahil <sahilrohera10@gmail.com>
2024-06-05 22:31:02 +0530

    [FEAT] workers for email-queue done

---
+++ index.js
@@ -0,0 +1,37 @@
+const { Worker } = require("bullmq");
+const IORedis = require("ioredis");
+
+require("dotenv").config({ path: `${process.cwd()}/.env` });
+
+const send_mail = () =>
+  new Promise((resolve, reject) => setTimeout(() => resolve(), 5 * 1000));
+
+console.log("bullmq=>", process.env.REDIS_BULLMQ_URL);
+const connection = new IORedis(process.env.REDIS_BULLMQ_URL || "", {
+  maxRetriesPerRequest: null,
+});
+
+const worker = new Worker(
+  "email-queue",
+  async (job) => {
+    console.log("A message recieved on worker 1 for job id => ", job.id);
+    console.log("Processing Message and sending mail .............");
+    await send_mail();
+    console.log("Email sent ....");
+  },
+  {
+    connection,
+  }
+);
+const worker2 = new Worker(
+  "email-queue",
+  async (job) => {
+    console.log("A message recieved on worker 2 for job id => ", job.id);
+    console.log("Processing Message and sending mail .............");
+    await send_mail();
+    console.log("Email sent ....");
+  },
+  {
+    connection,
+  }
+);
